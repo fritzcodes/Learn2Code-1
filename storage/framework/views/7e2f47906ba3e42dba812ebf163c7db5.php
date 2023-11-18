035b216c6f48cd40894e27cfdc621c46f82d61e0
fritzpogi123 <125795611+fritzpogi123@users.noreply.github.com>
2023-11-19 04:44:52 +0800

    Add files via upload

---
+++ storage/framework/views/7e2f47906ba3e42dba812ebf163c7db5.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Welcome</title>
+<link rel="stylesheet" href="assets/css/landing.css">
+ <link rel="stylesheet"
+   href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
+
+ <link rel="preconnect" href="https://fonts.googleapis.com">
+ <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+ <link href="https://fonts.googleapis.com/css2?family=Orbitron:wght@400;600;800&display=swap" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@400;500&display=swap" rel="stylesheet">
+
+</head>
+<body >
+<div class="body"style="background-image:url(assets/images/backg.svg)">
+<!--header-->
+<header>
+<!--logo-->
+<a href="#" class="logo"><img src="assets/images/logo.svg" style="width: 20px height; height: 40px; align-content: center;"></a>
+<div class="bx bx-menu" id="menu-icon"></div>
+<!--navbar-->
+<ul class="navbar">
+<li><a href="#home">Home</a></li>
+<li><a href="#features">Features</a></li>
+<li><a href="#about">About</a></li>
+<li><a href="login">Login</a></li>
+</ul>
+</header>
+
+<!--Home section-->
+<section class="landing" id="landing">
+<div class="landing-text">
+<h1>Ready<br>Set<br><span style="color: #8367AB;">CODE!</span></h1>
+<p>Gamified Coding Platform for Everyone!</p>
+<a href="#" class="home-btn"><img src="assets/images/start.svg" width="150px" height="20%"></a>
+</div>
+</section>
+
+<section class="gamified" id="gamified">
+<div class="play">
+<h2>Gamified Features</h2>
+<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+
+<a href="#" class="play-btn"><img src="assets/images/playbtn.svg" width="150px" height="20%"></a>
+</div>
+</section>
+
+</div>
+
+</body>
+</html>
+<?php /**PATH C:\xampp\htdocs\laravelpractice\resources\views/frontend/landing.blade.php ENDPATH**/ ?>
